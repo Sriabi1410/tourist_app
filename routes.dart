9e241aca959323f1f15a5f9b665ff02f922602b5
subhasini-0106 <subhavijay638@gmail.com>
2026-04-02 21:28:17 +0530

    Delete routes.dart

--- routes.dart
+++
@@ -1,9 +0,0 @@
-class Routes {
-  static const login = '/login';
-  static const register = '/register';
-  static const dashboard = '/dashboard';
-  static const emergencySos = '/sos';
-  static const locationTracking = '/tracking';
-  static const emergencyContacts = '/contacts';
-  static const settings = '/settings';
-}
